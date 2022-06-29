a6e52ef6228ba0393202918391a4c4c00ed19c5d
tranminhvu22 <minhvu27092001@gmail.com>
2022-06-29 14:14:31 +0700

    fix state

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -5,23 +5,22 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private const float A = -9.8f;
+    private const float g = -9.8f;
     private float _v0;
+    private float _v;
     private float _s0;
     private float _t = 0;
-    private float _v;
     private float yFall = 0;
-
-    [SerializeField] private GamePlay disks;
-    // Start is called before the first frame update
+    private float yJump=0;
     private State _currentState = State.Jump;
+    [SerializeField] private GamePlay disks;
     enum State
     {
         Jump,Fall,Smash,Die
     }
     void Start()
     {
-
+        
     }
     // Update is called once per frame
     void Update()
@@ -29,31 +28,30 @@ public class Ball : MonoBehaviour
         switch (_currentState)
         {
             case State.Jump:
-                //TODO: Tinh toan vi tri dung cong thuc roi tu do
-                _v += A * Time.deltaTime;
-                yFall = _s0 + _v0 * _t + A * _t * _t * 0.5f;
-                transform.position = new Vector3(0, yFall, 2.5f);
+                //TODO: tinh do nay cua qua bong
+                _v -= g * Time.deltaTime;
+                yJump = _s0 + _v *_t + 0.5f*g * _t * _t;
+                transform.position = new Vector3(0, yJump, 2.5f);
+                // transform.localScale = new Vector3(1, 0.2f, 1);
                 //TODO: Kiem tra xem qua bong da len den dinh hay chua
-                if(disks.GetTopLayer().transform.position.y+0.5f <= 2f)
-                {  
+                if(disks.DiskList[0].transform.position.y + yJump > 3)
+                {
                     ChangeSate(State.Fall);
                     return;
                 }
                 break;
             case State.Fall:
-                // TODO: Kiem tra xem qua bong co va cham vao layer tren cung hay khong
-                _v += A * Time.deltaTime;
-                yFall = _s0 + _v0 * _t + A * _t * _t * 0.5f;
+                _v += g * Time.deltaTime;
+                yFall = _s0 + _v *_t + 0.5f*g * _t * _t;
                 transform.position = new Vector3(0, yFall, 2.5f);
-                if (disks.GetTopLayer().transform.position.y - 2f <= yFall)
+                // transform.localScale = new Vector3(0.2f,1,0.2f)*Time.deltaTime;
+                if (disks.DiskList[0].transform.position.y + yFall < 1)
                 {
                     ChangeSate(State.Jump);
                     return;
                 }
-                // // TODO: Kiem tra xem qua bong co va cham vao layer tren cung hay khong
                 break;
             case State.Smash:
-                
                 break;
             case State.Die:
                 break;
@@ -70,13 +68,13 @@ public class Ball : MonoBehaviour
         switch (state)
         {
             case State.Jump:
-                _s0 = transform.position.y;
-                _v0 = _v;
+                _s0 = disks.DiskList[0].transform.position.y+1f;
+                _v = 5f;
                 _t = 0;
                 break;
             case State.Fall:
                 _s0 = transform.position.y;
-                _v0 = 0.0f;
+                _v = 0;
                 _t = 0;
                 break;
             case State.Smash:
--- Assets/Script/GamePlay.cs
+++ Assets/Script/GamePlay.cs
@@ -9,53 +9,64 @@ public class GamePlay : MonoBehaviour
     public List<GameObject> DiskList = new List<GameObject>();
     [SerializeField] private GameObject ObDisk;
     private GameObject firstDisk;
-    private int platformAmount = 20 ;
     private GameStates _currentGameState = GameStates.Idle;
-    public static GamePlay Instance {
-        get;
-        private set;
-    }
+    private MeshRenderer[] myRenderer;
+    [SerializeField] private Material myColor;
+    public static GamePlay Instance { get; private set; }
 
     private void Awake()
     {
         Instance = this;
+        myRenderer = GetComponentsInChildren<MeshRenderer>();
+        
     }
+
     private enum GameStates
     {
-        Idle, Smash, Win, Lose
+        Idle,
+        Smash,
+        Win,
+        Lose
     }
 
     private void Start()
     {
         CreatDisk();
+        MeshColor();
+        DiskList[0].SetActive(false);
     }
 
     public void Update()
     {
-       
     }
+
     //Start make Disk
     public void CreatDisk()
     {
-        for(int i = 1; i <= platformAmount ; i++)
+        for (int i = 0; i <= 20; i++)
         {
-            GameObject cloneDisk = Instantiate(ObDisk, new Vector3(0,  i + 0.5f, 0), Quaternion.Euler(new Vector3(0, i*10, 0)));
+            GameObject cloneDisk = Instantiate(ObDisk, new Vector3(0, i * -1.5f, 0),
+                Quaternion.Euler(new Vector3(0, i * 10, 0)));
             DiskList.Add(cloneDisk);
         }
     }
-    
-    public GameObject GetTopLayer()
-    { 
-        Debug.Log("abc");
-        firstDisk = DiskList[platformAmount];
-        return firstDisk;
-    }
-    //End make Disk
-    
     //Start make Level
-    
+
     //End make Level
     
+    //Color Black piece disk
+    private void MeshColor()
+    {
+        foreach (var mesh in myRenderer)
+        {
+            if (mesh.gameObject.transform.parent.CompareTag("Black_Piece"))
+            {
+                var mat = mesh.materials;
+                mat[0] = myColor;
+                mesh.materials = mat;
+            }
+        }
+    }
     private void ChangeState(GameStates newState)
     {
         if (newState == _currentGameState) return;
@@ -63,6 +74,7 @@ public class GamePlay : MonoBehaviour
         _currentGameState = newState;
         EnterNewState();
     }
+
     private void EnterNewState()
     {
         switch (_currentGameState)
@@ -79,6 +91,7 @@ public class GamePlay : MonoBehaviour
                 throw new ArgumentOutOfRangeException();
         }
     }
+
     private void ExitCurrentState()
     {
         switch (_currentGameState)
