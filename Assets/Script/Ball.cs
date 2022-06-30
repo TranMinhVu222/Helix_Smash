514235f9e911bdfb3b85e29b198ae5d304fed2a3
tranminhvu22 <minhvu27092001@gmail.com>
2022-07-01 00:00:01 +0700

    done: Smash and Destroy ing

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -5,47 +5,60 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private const float g = -9.8f;
-    private float _v0;
+    private const float g = 9.8f;
+    private const float _v0 = 3.5f;
     private float _v;
-    private float _s0;
+    // private float _smax = g * _v0;
+    private float _smax = 3f;
     private float _t = 0;
     private float yFall = 0;
-    private float yJump=0;
+    private float yJump =0;
+    private float _s0 = 0;
+    private float _scaleJump = 0;
+    private float _scaleFall = 0;
     private State _currentState = State.Jump;
     [SerializeField] private GamePlay disks;
     enum State
     {
         Jump,Fall,Smash,Die
     }
+
     void Start()
     {
-        
     }
+
     // Update is called once per frame
     void Update()
     {
         switch (_currentState)
         {
             case State.Jump:
-                //TODO: tinh do nay cua qua bong
-                _v -= g * Time.deltaTime;
-                yJump = _s0 + _v *_t + 0.5f*g * _t * _t;
+                //LAM CHO QUA BONG NHAY LEN
+                yJump = _s0 + _v * _t + 0.5f * g * _t * _t;
                 transform.position = new Vector3(0, yJump, 2.5f);
-                // transform.localScale = new Vector3(1, 0.2f, 1);
-                //TODO: Kiem tra xem qua bong da len den dinh hay chua
-                if(disks.DiskList[0].transform.position.y + yJump > 3)
+                Debug.Log(("Jumping: " + yJump));
+                //LAM CHO QUA BONG BIEN DANG KHI NHAY LEN
+                _scaleJump = yJump/(_t*200) ;
+                Debug.Log(_scaleJump);
+                transform.localScale = new Vector3((float)(1 - 0.5 * _scaleJump), (float)(1 + 0.5 * _scaleJump), (float)(1 - 0.5 * _scaleJump));
+                //PHAT HIEN THOI DIEM ROI XUONG
+                if(yJump > _smax)
                 {
                     ChangeSate(State.Fall);
                     return;
                 }
                 break;
             case State.Fall:
-                _v += g * Time.deltaTime;
-                yFall = _s0 + _v *_t + 0.5f*g * _t * _t;
+                //LAM CHO QUA BONG ROI XUONG
+                yFall = _s0 + 0.5f * -g * _t * _t;
                 transform.position = new Vector3(0, yFall, 2.5f);
-                // transform.localScale = new Vector3(0.2f,1,0.2f)*Time.deltaTime;
-                if (disks.DiskList[0].transform.position.y + yFall < 1)
+                Debug.Log("Falling: " + yFall);
+                //LAM CHO QUA BONG BIEN DANG KHI ROI
+                _scaleFall = yFall/(_t*300);
+                transform.localScale = new Vector3(1f, (float)(1 - 0.5 * _scaleFall), 1);
+                Debug.Log(_scaleFall);
+                //PHAT HIEN THOI DIEM NHAY LEN
+                if (yFall < disks.DiskList[0].transform.position.y + 1f)
                 {
                     ChangeSate(State.Jump);
                     return;
@@ -58,6 +71,7 @@ public class Ball : MonoBehaviour
             default:
                 throw new ArgumentOutOfRangeException();
         }
+
         _t += Time.deltaTime;
     }
 
@@ -69,7 +83,7 @@ public class Ball : MonoBehaviour
         {
             case State.Jump:
                 _s0 = disks.DiskList[0].transform.position.y+1f;
-                _v = 5f;
+                _v = _v0;
                 _t = 0;
                 break;
             case State.Fall:
